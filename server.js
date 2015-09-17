bac7645d5d46228b02e7b46ea441dc6cf1bcfebc
Oliver <o.lucas@live.com>
2015-09-17 15:28:59 +0100

    added extraction of title and release dates

--- server.js
+++ server.js
@@ -15,6 +15,18 @@ app.get('/scrape', function(req, res){
 
       var title, release, rating;
       var json = { title: "", release : "", rating : ""}
+
+      $('.header').filter(function(){
+
+        var data = $(this);
+
+        title = data.children().first().text();
+        release = data.children().last().children().text();
+
+        json.title = title;
+        json.release = release;
+
+      });
     }
   });
 
